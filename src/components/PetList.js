e3c182c8fb1eace1ac1a8322d8811fb27b78b4be
sriramede9 <only4programming03@gmail.com>
2025-03-15 20:59:20 -0400

    api call to load all pets

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -1,11 +1,29 @@
-// src/components/PetList.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import PetCard from './PetCard';
-import { pets } from '../data/Pet';
 
 function PetList() {
+    const [pets, setPets] = useState([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        console.log("Fetching pets data...");
+        axios.get('http://localhost:9900/api/pets')
+            .then(res => {
+                console.log("result", res);
+                setPets(res.data);
+            })
+            .catch(err => {
+                console.error("Error fetching pets data:", err);
+                setError(err.message);
+            });
+    }, []); // Empty dependency array
+
+    console.log("PetList component rendered");
+
     return (
         <div className="pet-list">
+            {error && <p className="text-danger">{error}</p>}
             {pets.map(pet => (
                 <PetCard key={pet.id} pet={pet} />
             ))}
