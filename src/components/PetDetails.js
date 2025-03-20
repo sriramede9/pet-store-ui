501b3028a9da4246e0c61abbdceef31f283dfc61
sriramede9 <only4programming03@gmail.com>
2025-03-20 00:22:09 -0400

    adding details corrected

--- src/components/PetDetails.js
+++ src/components/PetDetails.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import PetCard from './PetCard';
-import PetDetails from './PetDetails';
+
 
 function PetDetails({pet}) {
     return (
         <div>
-                <PetCard key={pet.id} pet={pet}  />
+            <h2>{pet.name}</h2>
+            <p>Breed: {pet.breed}</p>
+            <p>Price: ${pet.price}</p>
+            <img src={pet.imageUrl} alt={pet.name} />
         </div>
     );
 }
