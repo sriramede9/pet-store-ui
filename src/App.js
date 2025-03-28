e49a4bcb162094e4a31e7d34d7c359327840d924
sriramede9 <only4programming03@gmail.com>
2025-03-27 23:06:07 -0400

    button fix

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { Routes, Route } from 'react-router-dom';
 import ListGroup from './components/ListGroup';
-import { Button } from 'bootstrap';
+import  Button  from './components/Button';
 
 function App() {
     return (
         <Routes>
                 {/* <Route path="/" element={<><PetList /><Footer year={2025} /></>} />
                 <Route path="/pet/:id" element={<PetDetails />} /> */}
-                <Route path="/" element={<><ListGroup /> <Button/></>} />
+                <Route path="/" element={<Button/>} />
         </Routes>
     );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,12 @@
-function Button() {
-  return <button className="btn btn-primary ">Click me</button>;
+import React from 'react';
+
+function handleOnClick() {
+  console.log('Button clicked!');
+  alert('Button clicked!');
 }
+// Define the Button component
+function Button() {
+  return <button className="btn btn-primary " onClick={handleOnClick}>Click me</button>;
+}
+
 export default Button;
